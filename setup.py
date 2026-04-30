3d3e7c7c938acf971d8029ce50618cf3b55337d1
sajib713 <sajib154347@gmail.com>
2026-05-01 02:20:10 +0600

    load data

---
+++ setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="us_visa",
+    version="0.0.0",
+    author="Sajib",
+    author_email="sajib154347@gmail.com",
+    packages=find_packages()
+)
